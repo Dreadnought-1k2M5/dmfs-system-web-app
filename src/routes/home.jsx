1a18775b2e7ab1d110bca0eed06761ecbe6388e0
James Carl P. Galapon <jc.galapon0314@student.tsu.edu.ph>
2022-10-15 02:00:57 +0800

    Flexboxed files, witch Icons

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -4,6 +4,12 @@ import UploadFile from "./UploadFile";
 
 import "./routes-css/home.css"
 
+import word from '../ms-word.png'
+
+import pdf from '../pdf.png'
+
+import file from '../file.png'
+
 const initialState = {
   listFiles: []
 }
@@ -38,14 +44,31 @@ function Home({userInstance}) {
 
   },[])
 
+    
+  function fileIcon(props){
+
+      console.log("Function is Used!");
+      console.log(props.split('.').pop() == "docx");
+      if(props.split('.').pop() === "docx"){
+        return (<>
+          <img src={word} alt="wind icon" className="icon"/>           
+        </>);
+      }
+      else if(props.split('.').pop() == "pdf"){
+        return <>
+          <img src={pdf} alt="wind icon" className="icon"/>           
+        </>;
+      };
+  };
+      
+
+
 /*   async function DownloadHandle(cid){
     let cid_temp = cid;
     let localFilename;
-
     await userInstance.get('fileObjectList').get(`${cid}`).on(data => {
       localFilename = data.filenameProperty;
     });
-
     fetch(`https://${cid_temp}.ipfs.w3s.link/ipfs/${cid_temp}/${localFilename}`).then(res => {
       let result = res.blob();
       console.log(result);
@@ -73,42 +96,37 @@ function Home({userInstance}) {
             </h3>
             <p>NOTE: Metadata of each file is transmitted across GUN peers, so you may see duplicates of the same file listed below.</p>
             <br></br>
-              <table className="table-container">
-                <thead className="table-row-container">
-                  <tr>
-                    <th>Name</th>
-                    <th>Content Identifier (CID)</th>
-                  </tr>
-                </thead>
-                <tbody>
+                
                  {/*  {setDuplicatesRemoved(...new Set(fileListState))} */}
-                    {fileListState.map(elem =>
-                    <tr>
-                      <td>{elem.filename}</td>
-                      <td>{elem.cid}</td>
-                      <td><button className="download-btn" onClick={async ()=>{
-                        let cid_temp = elem.cid;
-                        let localFilename = elem.filename;
-                    
-                        fetch(`https://${cid_temp}.ipfs.w3s.link/ipfs/${cid_temp}/${localFilename}`).then(res => {
-                          let result = res.blob();
-                          console.log(result);
-                          return result;
-                        }).then(res => {
-                          const aElement = document.createElement('a');
-                          aElement.setAttribute('download', `${localFilename}`);
-                          const href = URL.createObjectURL(res);
-                          aElement.href = href;
-                          aElement.setAttribute('target', '_blank');
-                          aElement.click();
-                          URL.revokeObjectURL(href);
-                        })
-                      }}>Download File</button></td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-              
+                  <div className="file-container">
+                   {fileListState.map(elem =>                 
+                        <div className="files">                                               
+                            {fileIcon(elem.filename)}
+                            {console.log(elem.filename)}
+                            {elem.filename}       
+                            {/* {elem.cid} */}
+                            
+                            <button className="download-btn" onClick={async ()=>{
+                            let cid_temp = elem.cid;
+                            let localFilename = elem.filename;
+                            console.log();
+                            fetch(`https://${cid_temp}.ipfs.w3s.link/ipfs/${cid_temp}/${localFilename}`).then(res => {
+                              let result = res.blob();
+                              console.log(result);
+                              return result;
+                            }).then(res => {
+                              const aElement = document.createElement('a');
+                              aElement.setAttribute('download', `${localFilename}`);
+                              const href = URL.createObjectURL(res);
+                              aElement.href = href;
+                              aElement.setAttribute('target', '_blank');
+                              aElement.click();
+                              URL.revokeObjectURL(href);
+                            })
+                          }}>Download</button>
+                        </div>               
+                    )}    
+                  </div>           
       </div>
     </div>
     

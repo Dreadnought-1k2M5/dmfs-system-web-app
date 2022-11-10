c5ebf9c7bf3b55e4215b1e3457eb062768e52aaa
Joshua Lumanog <lumanog.joshua05@gmail.com>
2022-11-11 05:33:10 +0800

    changed the URL for axios post request

--- src/routes/modal-components/UploadGroupModal.jsx
+++ src/routes/modal-components/UploadGroupModal.jsx
@@ -441,7 +441,7 @@ export default function UploadGroupModal({uuidRoomObj, gunInstance, userInstance
                 //let stringifiedShareHolderObject = JSON.stringify(shareHolderObject);
 
                 
-                 Axios.post("https://node-secret-share.herokuapp.com/secret", {
+                 Axios.post("https://floating-fjord-99601.herokuapp.com/secret", {
                         exportedKey: parsedExportedKey,
                         seaPairTeamRoom: seaPairTeamRoomParsed,
                         shareHolders: shareHolderObject
